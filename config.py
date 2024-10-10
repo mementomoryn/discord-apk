f91cbc83d019313ff84b6e4eedc2f2604171f84c
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-10-11 01:23:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 APKMIRROR_URL: str = "https://www.apkmirror.com/apk/discord-inc/discord-chat-for-gamers/"
 
+APKMIRROR_CLOUDFLARE: bool = False
+
 REVANCED_PATCH_REPOSITORY: str = "revanced/revanced-patches"
 
 REVANCED_INTEGRATION_REPOSITORY: str = "revanced/revanced-integrations"
@@ -7,5 +9,11 @@ REVANCED_INTEGRATION_REPOSITORY: str = "revanced/revanced-integrations"
 REVANCED_CLI_REPOSITORY: str = "inotia00/revanced-cli"
 
 XPOSED_MODULE_REPOSITORY: str = "revenge-mod/revenge-xposed"
+
+LSPATCH_REPOSITORY: str = "JingMatrix/LSPatch"
+
+APKEDITOR_REPOSITORY: str = "REAndroid/APKEditor"
+
+APKRENAMER_REPOSITORY: str = "dvaoru/ApkRenamer"
 
 ENABLED_BINS: list = ["xposed", "apkeditor", "apkrenamer"]
