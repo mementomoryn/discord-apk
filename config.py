068c1bb0fe03f6c7673c0a42aadd3e6731c68870
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-10-11 22:54:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 APKMIRROR_URL: str = "https://www.apkmirror.com/apk/discord-inc/discord-chat-for-gamers/"
 
-APKMIRROR_CLOUDFLARE: bool = False
-
 REVANCED_PATCH_REPOSITORY: str = "revanced/revanced-patches"
 
 REVANCED_INTEGRATION_REPOSITORY: str = "revanced/revanced-integrations"
